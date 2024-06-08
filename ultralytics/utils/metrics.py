7bc65298463eaaada90326920b4de0cdea22614c
XiaoJiNu <15556461163@163.com>
2024-06-08 09:36:41 +0800

    数据载入和loss梳理

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -68,6 +68,7 @@ def box_iou(box1, box2, eps=1e-7):
     box1 = torch.as_tensor(box1, dtype=torch.float32)
     box2 = torch.as_tensor(box2, dtype=torch.float32)
     # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    # 总的来说，这段代码的目的是计算两组边界框的交集面积
     (a1, a2), (b1, b2) = box1.unsqueeze(1).chunk(2, 2), box2.unsqueeze(0).chunk(2, 2)
     inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp_(0).prod(2)
 
